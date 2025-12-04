81d482b1748ea60d4ecd043b2867ec72aa3493c1
Pablo Aristizabal <pablo.aristizabal.p@gmail.com>
2025-12-04 13:12:01 +0000

    RandomCardGenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,34 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+//write your code here
+const valores = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const palos = ["♥", "♦", "♣", "♠",];
+const colors = ["red", "black"]
+
+function RandomGenerator() {
+  const valor = valores[Math.floor(Math.random() * valores.length)];
+  const palo = palos[Math.floor(Math.random() * palos.length)];
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  return { valor, palo, color };
+}
+
+function NewCard() {
+  const RandomCard = RandomGenerator();
+  document.getElementById("topLeft").innerHTML = RandomCard.palo;
+  document.getElementById("bottomRight").innerHTML = RandomCard.palo;
+  document.getElementById("centerSymbol").innerHTML = RandomCard.valor;
+  document.getElementById("card").style.color = RandomCard.color;
+  return NewCard;
+}
+
+document.getElementById("button").onclick = NewCard()
+
+
+
+
+
+
+
+
